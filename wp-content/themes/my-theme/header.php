09517c8efb74eca9cd310fd49bd1f87dd6c054c9
YaMe09 <134693503+YaMe09@users.noreply.github.com>
2025-03-25 07:47:30 +0100

    fix style

--- wp-content/themes/my-theme/header.php
+++ wp-content/themes/my-theme/header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <?php wp_head(); ?> <!-- WordPress head hook -->
 </head>
 <body <?php body_class(); ?>>
